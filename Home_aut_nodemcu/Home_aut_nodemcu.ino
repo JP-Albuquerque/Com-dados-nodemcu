a0bef8923976d8a6de658f878ee5ca5c3e3237ff
jpbskt <jpbskt@hotmail.com>
2017-07-04 09:26:10 -0300

    Consertado problemas com sensor infravermelho

--- Home_aut_nodemcu/Home_aut_nodemcu.ino
+++ Home_aut_nodemcu/Home_aut_nodemcu.ino
@@ -2,8 +2,8 @@
 #include <DHT.h>
 #include <IRremoteESP8266.h> //INCLUSÃO DE BIBLIOTECA
  
-const char* ssid = "net virtua 1450 apto 701";
-const char* password = "juniorlia1";
+const char* ssid = "MotoG3";
+const char* password = "qwertyui";
 
 //defines - mapeamento de pinos do NodeMCU
 #define D0    16
@@ -28,20 +28,28 @@ IRsend irsend(12); //FUNÇÃO RESPONSÁVEL PELO MÉTODO DE ENVIO DO SINAL IR / U
 int tamanho = 68; //TAMANHO DA LINHA RAW(68 BLOCOS)
 int frequencia = 32; //FREQUÊNCIA DO SINAL IR(32KHz)
 
+//Variáveis usadas para calcular o tempo de execução
+unsigned long tic;  
+unsigned long toc;
+
+//Dados Raw para infravermelho
 // BOTÃO LIGA / DESLIGA
-unsigned int LD[68]= {9150,4500,650,600,650,600,650,1700,650,600,600,600,650,600,650,600,650,600,650,1700,650,1700,600,600,650,1700,650,1700,650,1700,600,1750,650,1700,650,600,650,600,600,600,600,1750,600,600,650,600,650,600,600,600,600,1750,650,1700,600,1750,650,600,600,1750,600,1750,650,1700,650,1700,600};
-// BOTÃO VOL+
-unsigned int VUP[68] = {9150,4450,650,500,650,500,650,1650,650,550,650,500,600,600,650,500,650,500,650,1650,700,1650,650,500,650,1650,700,1650,650,1650,700,1650,650,1650,650,500,650,1650,650,550,650,500,650,500,650,500,650,500,650,500,650,1650,650,500,650,1650,650,1650,650,1650,650,1650,600,1700,650,1650,650};
-// BOTÃO VOL-
-unsigned int VDW[68] = {9150,4450,700,500,700,500,700,1650,700,500,700,500,700,500,700,500,700,550,650,1650,700,1650,700,500,700,1650,700,1650,700,1650,700,1650,700,1650,700,1650,700,1650,700,500,700,550,650,550,650,500,650,500,700,500,700,500,700,500,700,1650,700,1650,700,1650,700,1650,700,1650,700,1650,700};
-// BOTÃO Canal+
-unsigned int CUP[68] = {9150,4450,700,500,700,550,650,1650,700,500,700,500,700,500,700,500,700,500,700,1650,700,1650,700,500,700,1650,700,1650,700,1650,700,1650,700,1650,700,500,700,500,700,500,700,500,700,500,700,500,700,500,700,500,700,1650,700,1650,700,1650,700,1650,700,1650,700,1650,700,1650,700,1650,700};
-// BOTÃO CANAL-
-unsigned int CDW[68] = {9150,4450,650,500,600,600,650,1650,700,500,650,500,600,600,650,550,650,500,650,1650,650,1650,650,500,650,1650,600,1700,650,1650,650,1650,650,1650,650,1650,650,500,650,550,650,500,650,500,650,550,650,550,650,500,650,500,650,1650,650,1650,700,1650,650,1650,650,1650,650,1650,700,1650,650};
+unsigned int LD[68]= {9000,4550,650,1700,600,1750,600,650,600,650,600,650,650,550,650,600,650,1700,650,1700,650,600,650,1700,600,650,650,1650,650,600,600,1750,650,600,650,600,650,600,650,600,650,600,650,1700,600,650,650,600,650,1700,650,1700,650,1700,650,1700,650,1700,650,600,650,1700,600,1750,650,600,650};
+// BOTÃO CIMA
+unsigned int UP[68] = {8950,4600,600,1750,650,1700,600,650,600,650,600,650,600,650,650,600,650,1700,650,1700,600,650,600,1750,600,650,600,1750,600,650,600,1750,600,600,600,650,600,650,650,600,600,650,600,1750,600,1750,650,600,600,1750,600,1750,600,1750,600,1700,650,1700,600,650,600,650,600,1750,650,600,600};
+// BOTÃO BAIXO
+unsigned int DOWN[68] = {9000,4550,600,1750,600,1750,600,650,550,650,600,650,600,650,600,650,600,1750,600,1750,600,650,600,1750,600,650,600,1700,600,600,600,1700,600,600,600,650,600,1750,600,650,600,650,600,1750,600,1750,600,650,600,1700,600,1750,600,650,600,1750,600,1750,600,650,600,650,600,1750,600,650,600};
+// BOTÃO ESQUERDA
+unsigned int LEFT[68] = {8950,4600,600,1750,600,1750,600,650,600,650,600,650,600,650,600,600,600,1700,600,1750,600,650,600,1750,600,650,600,1750,600,650,600,1750,600,650,600,1750,600,1750,600,600,600,650,600,1750,600,1750,600,650,600,1750,600,650,600,650,600,1750,600,1750,600,650,600,650,600,1750,600,650,600};
+// BOTÃO OK
+unsigned int OKK[68] = {8900,4550,600,1750,600,1750,600,650,600,650,600,650,600,650,600,650,600,1750,600,1750,600,700,550,1750,600,650,600,1750,600,600,600,1700,600,600,600,1700,600,650,600,1750,600,650,600,600,600,650,600,650,600,1750,600,650,600,1750,600,650,600,1750,600,1750,600,1750,600,1750,600,650,600};
+// BOTÃO MENU
+unsigned int MENU[68] = {8900,4600,550,1750,600,1750,600,650,600,650,600,600,600,600,600,600,600,1700,600,1700,600,600,600,1700,600,650,600,1750,600,650,600,1750,600,650,600,650,600,1750,600,650,600,1750,600,1750,600,650,600,650,600,1750,600,1750,600,650,600,1750,600,650,600,650,600,1750,600,1750,600,650,600};
+// BOTÃO MENU
+unsigned int NULO[68] = {0,0,0,0,0,0,0,0,0,0,0,0,0,600,600,600,600,1700,600,1700,600,600,600,1700,600,650,600,1750,600,650,600,1750,600,650,600,650,600,1750,600,650,600,1750,600,1750,600,650,600,650,600,1750,600,1750,600,650,600,1750,600,650,600,650,600,1750,600,1750,600,650,600};
 
-
-WiFiServer server(80);
-int presence;
+WiFiServer server(80);      //Porta usada para se comunicar no WIFI
+int presence;               //Variável usada para controle do sensor de presença
 
   //Sensor DHT22
   DHT dht(DHTPIN, DHTTYPE,11); //Criação do objeto de leitura DHT
@@ -54,18 +62,32 @@ int presence;
 void setup() {
   irsend.begin();
   Serial.begin(115200);
+  dht.begin(); //Inicia o sensor
   delay(10);
  
-  pinMode(D7, OUTPUT);
-  digitalWrite(D7, LOW);
+  pinMode(D7, OUTPUT);                //Inicializamos o GPIO2 como saída
+  digitalWrite(D7, LOW);              //Led ligado no GPIO2 começa apagado
+  pinMode(D5,INPUT);                      //Recebe sinal do sensor de presença
+  pinMode(D4,INPUT);                  //Chave que troca o Nodemcu de AP para Station
+  pinMode(D3,OUTPUT);                     //Controla led do sensor de presença
+  pinMode(D1,OUTPUT);                     //Controla led do ldr
 
-  pinMode(D5, INPUT);
-
-  pinMode(D1, OUTPUT);
-
-  dht.begin(); //Inicia o sensor
+  int R=digitalRead(D4);              //Faz a leitura da chave que troca o nodemcu para AP
   
+  if (R==LOW){                        //Caso a chave esteja em sinal baixo (zero de tensão), configura modo AP
+  server.begin();                         //inicializamos o servidor
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ssid);                    //Rede aberta
  
+  Serial.println();
+ 
+  Serial.print("Direccion IP Access Point - por defecto: ");      //Imprime la dirección IP
+  Serial.println(WiFi.softAPIP()); 
+  Serial.print("Direccion MAC Access Point: ");                   //Imprime la dirección MAC
+  Serial.println(WiFi.softAPmacAddress()); 
+ 
+    }
+  if(R==HIGH){                        //Caso a chave esteja em sinal alto (3.3V), configura modo station
   // Connect to WiFi network
   Serial.println();
   Serial.println();
@@ -76,8 +98,11 @@ void setup() {
  
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
+    digitalWrite(LED_BUILTIN,!digitalRead(LED_BUILTIN));
     Serial.print(".");
   }
+
+  digitalWrite(LED_BUILTIN,HIGH);
   Serial.println("");
   Serial.println("WiFi connected");
  
@@ -90,10 +115,12 @@ void setup() {
   Serial.print("http://");
   Serial.print(WiFi.localIP());
   Serial.println("/");
+  }
  
 }
  
 void loop() {
+  tic = millis(); 
   // Check if a client has connected
   WiFiClient client = server.available();
   if (!client) {
@@ -125,18 +152,19 @@ void loop() {
   // Controle do sensor de presença
   if (digitalRead(D5) == LOW) {
     presence = 0;
+    digitalWrite(D3, LOW);
   } else  {
     presence = 1;
+    digitalWrite(D3, HIGH);
   }
 
   // Controla sensor LDR
   ldrValor = analogRead(ldrPin);
-  if(ldrValor > 500 & presence == 1) {
+  if(ldrValor > 500) {
     digitalWrite(D1, HIGH);
   } else  {
     digitalWrite(D1, LOW);
   }
-
   Serial.print("LDR ");
   Serial.println(ldrValor);
 
@@ -145,52 +173,54 @@ void loop() {
   h=dht.readHumidity();//Função de leitura da umidade
 
   // Controle do infravermelho
-  // Read the first line of the request
-  String request1 = client.readStringUntil('\r');
-  Serial.print("request ");
-  Serial.println(request1);
-  client.flush();
- 
-  // Condição que liga / desliga TV
+  // Condição que liga / desliga projetor
   if (request.indexOf("/LIGA") != -1)  {
     irsend.sendRaw(LD,tamanho,frequencia);  // PARÂMETROS NECESSÁRIOS PARA ENVIO DO SINAL IR
     Serial.println("Comando enviado: Liga / Desliga");
     delay(50); // TEMPO(EM MILISEGUNDOS) DE INTERVALO ENTRE UM COMANDO E OUTRO
   }
-  // Função que aumenta o volume da TV
-  if (request.indexOf("/VOL+") != -1)  {
-        irsend.sendRaw(VUP,tamanho,frequencia); // PARÂMETROS NECESSÁRIOS PARA ENVIO DO SINAL IR
+  
+  // Condição botão seta para cima
+  if (request.indexOf("/CIMA") != -1)  {
+        irsend.sendRaw(UP,tamanho,frequencia); // PARÂMETROS NECESSÁRIOS PARA ENVIO DO SINAL IR
         Serial.println("Comando enviado: Aumenta volume");
         delay(50); // TEMPO(EM MILISEGUNDOS) DE INTERVALO ENTRE UM COMANDO E OUTRO
   }
-  // Função que diminui o volume da TV
-  if (request.indexOf("/VOL-") != -1)  {
-        irsend.sendRaw(VDW,tamanho,frequencia); // PARÂMETROS NECESSÁRIOS PARA ENVIO DO SINAL IR
+  // Condição botão seta para baixo
+  if (request.indexOf("/BAIXO") != -1)  {
+        irsend.sendRaw(DOWN,tamanho,frequencia); // PARÂMETROS NECESSÁRIOS PARA ENVIO DO SINAL IR
         Serial.println("Comando enviado: Abaixa volume");
         delay(50); // TEMPO(EM MILISEGUNDOS) DE INTERVALO ENTRE UM COMANDO E OUTRO
   }
-  // Função que aumenta o canal da TV
-  if (request.indexOf("/CUP") != -1)  {
-        irsend.sendRaw(CUP,tamanho,frequencia); // PARÂMETROS NECESSÁRIOS PARA ENVIO DO SINAL IR
+  // Condição botão seta para esquerda
+  if (request.indexOf("/ESQUERDA") != -1)  {
+        irsend.sendRaw(LEFT,tamanho,frequencia); // PARÂMETROS NECESSÁRIOS PARA ENVIO DO SINAL IR
         Serial.println("Comando enviado: Aumenta canal");
         delay(50); // TEMPO(EM MILISEGUNDOS) DE INTERVALO ENTRE UM COMANDO E OUTRO
   }
-  // Função que diminue o canal da TV
-  if (request.indexOf("/CDW") != -1)  {
-        irsend.sendRaw(CDW,tamanho,frequencia); // PARÂMETROS NECESSÁRIOS PARA ENVIO DO SINAL IR
+  // Condição botão OK
+  if (request.indexOf("/OK") != -1)  {
+        irsend.sendRaw(OKK,tamanho,frequencia); // PARÂMETROS NECESSÁRIOS PARA ENVIO DO SINAL IR
+        Serial.println("Comando enviado: Diminui canal");
+        delay(50); // TEMPO(EM MILISEGUNDOS) DE INTERVALO ENTRE UM COMANDO E OUTRO
+  }
+  // Condição botão menu
+  if (request.indexOf("/MENU") != -1)  {
+        irsend.sendRaw(MENU,tamanho,frequencia); // PARÂMETROS NECESSÁRIOS PARA ENVIO DO SINAL IR
         Serial.println("Comando enviado: Diminui canal");
         delay(50); // TEMPO(EM MILISEGUNDOS) DE INTERVALO ENTRE UM COMANDO E OUTRO
   }
   
-  
   //Parte HTML
   client.println("HTTP/1.1 200 OK");
-  //client.println("Content-Type: text/html");
-  //client.println("<meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\" />");
   client.println(""); //  do not forget this one
   client.println("<!DOCTYPE HTML>");
+  client.println("<meta charset='UTF-8'>");
   client.println("<html>");
-  client.println("<meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\" />");
+  client.println("<head>");
+  client.println("<meta http-equiv=\"refresh\" content=\"3\">");        //Faz atualização da página web de 3 em 3 segundos
+  client.println("</head>");
+  //client.println("<meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\" />");
   
   // Criação de caixa para apresentar o status do sensor de presença
   client.println("<table width=\"280\" cellspacing=\"1\" cellpadding=\"3\" border=\"0\" bgcolor=\"#1E679A\"> ");
@@ -202,12 +232,12 @@ void loop() {
   client.println("<tr>");
   client.println("<td bgcolor=\"#ffffcc\">"); 
   client.println("<font face=\"arial, verdana, helvetica\">");
-  
-  if(presence == 1) 
+  if(presence == 1) {
     client.println("Sensor de presença ligado!");
-   else if (presence == 0) 
+  }
+   else if (presence == 0){
     client.println("Sensor de presença desligado!");
-  
+   }
   client.println("</font> ");
   client.println("</td>");
   client.println("</tr>");
@@ -225,7 +255,6 @@ void loop() {
   client.println("<tr>");
   client.println("<td bgcolor=\"#ffffcc\">"); 
   client.println("<font face=\"arial, verdana, helvetica\">");
- 
   if(value == HIGH) {
     client.print("On");
   } else {
@@ -239,7 +268,6 @@ void loop() {
   client.println("</tr>");
   client.println("</table> ");
   client.println("<br><br>");
-  //client.println("</html>");
 
   // Criação de caixa para apresentar o status do sensor de luz
   client.println("<table width=\"280\" cellspacing=\"1\" cellpadding=\"3\" border=\"0\" bgcolor=\"#1E679A\"> ");
@@ -251,13 +279,11 @@ void loop() {
   client.println("<tr>");
   client.println("<td bgcolor=\"#ffffcc\">"); 
   client.println("<font face=\"arial, verdana, helvetica\">");
-  
-  if(ldrValor > 500 & presence == 1) {
+  if(ldrValor > 500) {
     client.println("Led do LDR ligado!");
   } else  {
     client.println("Led do LDR desligado!");
   }
-  
   client.println("</font> ");
   client.println("</td>");
   client.println("</tr>");
@@ -300,18 +326,20 @@ void loop() {
  
   client.println("<br><br>");
   client.println("<a href=\"/LIGA\"\"><button> On / Off </button></a><br/>");
-  client.println("<a href=\"/VOL+\"\"><button>VOLUME +</button></a>");
-  client.println("<a href=\"/VOL-\"\"><button>VOLUME -</button></a><br/>");
-  client.println("<a href=\"/CUP\"\"><button>CANAL +</button></a>");
-  client.println("<a href=\"/CDW\"\"><button>CANAL -</button></a><br/>");
+  client.println("<a href=\"/CIMA\"\"><button>CIMA</button></a>");
+  client.println("<a href=\"/BAIXO\"\"><button>BAIXO</button></a><br/>");
+  client.println("<a href=\"/ESQUERDA\"\"><button>ESQUERDA</button></a>");
+  client.println("<a href=\"/MENU\"\"><button>MENU</button></a>");
+  client.println("<a href=\"/OK\"\"><button>OK</button></a><br/>");
   client.println("</html>");
-  
  
   delay(1);
   Serial.println(t);
   Serial.println(h);
   Serial.println("Client disconnected");
   Serial.println("");
- 
+  
+  toc = millis() - tic;
+  Serial.println(toc);          //Imprime tempo de execução na serial
+
 }
- 
